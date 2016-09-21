3baed15bcb0b673bcb78cb4a4517055ead58c033
ryanvpham <ryanvpham94@gmail.com>
2016-09-21 09:59:55 -0700

    Update ProgrammingAssignment2.R

--- ProgrammingAssignment2.R
+++ ProgrammingAssignment2.R
@@ -1,6 +1,7 @@
 ### Cache a matrix and find the inverse if there is one
+a <- diag(3,2)
 
-#Create a matrix and cahce is outside of the function environment
+#Create a matrix and cache it outside of the function environment
 makeCacheMatrix <- function(x = matrix()) {
      m <- NULL
      set <- function(y) {
@@ -18,10 +19,19 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## Calculate the inverse of the matrix and print output
 
-CachedMarix <- makeCacheMatrix(a)
-cacheSolve(CachedMarix)
-
-set <- function(y) {
-     x <<- y
-     m <<- NULL
+cacheSolve <- function(x, ...) {
+     m <- x$getinverse()
+     if(!is.null(m)) {
+          message("getting cached data")
+          return(m)
+     }
+     data <- x$get()
+     m <- solve(data, ...)
+     x$setinverse(m)
+     m
 }
+
+
+CachedMatrix <- makeCacheMatrix(a)
+cacheSolve(CachedMatrix)
+
